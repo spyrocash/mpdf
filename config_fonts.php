c13df86297886e5f181b3dc2c1bba061f4b97b1c
spyrocash <spyrocash@gmail.com>
2016-09-20 11:07:34 +0700

    add font thai in mpdf

--- config_fonts.php
+++ config_fonts.php
@@ -81,6 +81,12 @@ $this->fonttrans = array(
  */
 
 $this->fontdata = array(
+	"thsarabunnew" => array(/* Thai */
+		'R' => "thsarabunnew-webfont.ttf",
+		'B' => "thsarabunnew_bold-webfont.ttf",
+		'I' => "thsarabunnew_italic-webfont.ttf",
+		'BI' => "thsarabunnew_bolditalic-webfont.ttf",
+	),
 	"dejavusanscondensed" => array(
 		'R' => "DejaVuSansCondensed.ttf",
 		'B' => "DejaVuSansCondensed-Bold.ttf",
